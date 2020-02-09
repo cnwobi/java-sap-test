6192286d807fc8498d2b7651877a40dfaa5db995
Chuka Nwobi <c.nwobi@outlook.com>
2020-02-09 21:00:56 +0100

    adding exceptions

---
+++ src/main/java/com/h2rd/refactoring/exception/EmailException.java
@@ -0,0 +1,6 @@
+package com.h2rd.refactoring.exception;
+public class EmailException extends Exception {
+    public EmailException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/h2rd/refactoring/service/UserService.java
+++ src/main/java/com/h2rd/refactoring/service/UserService.java
@@ -1,4 +1,15 @@
 package com.h2rd.refactoring.service;
 
+import com.h2rd.refactoring.usermanagement.User;
+
+import java.util.List;
+
 public interface UserService {
+    void saveUser(User user) throws Exception;
+    List<User> getUsers();
+    void deleteUser(User userToDelete) throws UserNotFoundException;
+    void updateUser(User userToUpdate) throws UserNotFoundException, RoleException, EmailException;
+    User findUserByEmail(String email) throws UserNotFoundException, EmailException;
+
+    List<User> findUsers(String name);
 }
